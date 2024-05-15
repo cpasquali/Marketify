29316ef5ef53fa0acd43fe6a8938677c71acbeb5
Constantino Pasquali <conipasquali2@gmail.com>
2024-05-14 22:28:32 -0300

    cambios en la nav

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -3,40 +3,46 @@ import './nav-bar.css';
 import { Link } from 'wouter';
 
 export const NavBar = () => {
-  const [opacityOn, setOpacityOn] = useState(true);
-  const [opacity, setOpacity] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-  const handleScroll = () => {
-    if (window.scrollY > 50) {
-        setOpacityOn(false);
+  const controlNavbar = () => {
+    if (window.scrollY > lastScrollY) {
+      setIsVisible(false);
     } else {
-        setOpacityOn(true);
+      setIsVisible(true);
     }
+    setLastScrollY(window.scrollY);
   };
 
-  const handleOpacityNav = () => {
-    setOpacity(!opacity)
-  }
-
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', controlNavbar);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('scroll', controlNavbar);
     };
-  }, []);
+  }, [lastScrollY]);
 
   return (
-    <header className={`navbar-container ${opacityOn ? "" : "header-opacity"} ${opacity ? "opacity-100" : ""}`}>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary position-fixed">
+    <header className={`navbar-container ${isVisible ? 'navbar-visible' : 'navbar-hidden'} position-fixed`}>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Marketify</a>
-          <button onClick={handleOpacityNav} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            onClick={() => setIsVisible(!isVisible)}
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link to={"/"} className="nav-link active" aria-current="page">Home</Link>
+                <Link to="/" className="nav-link active" aria-current="page">Home</Link>
               </li>
               <li className="nav-item dropdown movil">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -50,7 +56,7 @@ export const NavBar = () => {
                   <li><Link to="/products/category/women's clothing" className="dropdown-item">Women's Clothing</Link></li>
                 </ul>
               </li>
-              <li className="nav-item dropdown pc" onClick={() => handleOpacityNav(true)}>
+              <li className="nav-item dropdown pc">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Categories
                 </a>
